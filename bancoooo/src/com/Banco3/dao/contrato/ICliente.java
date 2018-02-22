67dcf6d10bd4e7ced921b5fb2ab2e3d0fe527b5b
Davidssito Campos <Davidssito Campos@DESKTOP-S8KPSSD.local>
2018-02-22 14:56:54 -0500

    David Campos ICliente

--- bancoooo/src/com/Banco3/dao/contrato/ICliente.java
+++ bancoooo/src/com/Banco3/dao/contrato/ICliente.java
@@ -17,5 +17,5 @@ public interface ICliente {
     public int modificar(Cliente cliente) throws Exception;
     public int eliminar(Cliente cliente) throws Exception;
     public Cliente obtener(String cedula) throws Exception;
-    public ArrayList<Cliente> obtener() throws Exception; 
+    public List<Cliente> obtener() throws Exception; 
 }
