412da0e0b01d75c7dcac9416cd660d119818da37
Jonathan Macas <Brayan Macas@FAMILIA-MACAS>
2018-02-22 09:38:08 -0500

    Actualizacion

--- bancoooo/src/com/Banco3/dao/contrato/ICliente.java
+++ bancoooo/src/com/Banco3/dao/contrato/ICliente.java
@@ -16,6 +16,6 @@ public interface ICliente {
     public int insertar(Cliente cliente) throws Exception;
     public int modificar(Cliente cliente) throws Exception;
     public int eliminar(Cliente cliente) throws Exception;
-    public Cliente obtener(int cliente) throws Exception;
+    public Cliente obtener(String cedula) throws Exception;
     public List<Cliente> obtener() throws Exception; 
 }
